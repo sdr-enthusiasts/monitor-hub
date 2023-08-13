42d5cc5b3a2e83b3c13d7601c2c41eff41163884
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2023-08-13 13:50:50 -0600

    webpack minimizer

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 const path = require("path");
 const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   module: {
     rules: [{ test: /.ts$/, use: "ts-loader" }],
   },
+  devtool: "source-map",
   entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.bundle.js",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({ parallel: true })],
   },
   plugins: [
     new webpack.ProvidePlugin({
